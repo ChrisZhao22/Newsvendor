83a0ccd312c9a9bb3522f665b9b8630223041f16
Zhao Wei <zhaowei@ZhaodeMacBook-Air.local>
2025-11-24 22:16:54 +0800

    some update

--- MultiBinPolySmoother(Chaudhuri 1991).py
+++ MultiBinPolySmoother(Chaudhuri 1991).py
@@ -4,7 +4,6 @@ from collections import defaultdict
 import numpy as np
 from scipy.optimize import linprog
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
 
 plt.rcParams['font.sans-serif'] = ['Arial Unicode MS', 'PingFang SC']
